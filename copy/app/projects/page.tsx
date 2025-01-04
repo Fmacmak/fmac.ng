2fd3fc5e23ef038a552b8f19bfe20fd54a1d0c66
Feranmi <fmacmak@gmail.com>
2025-01-05 00:04:36 +0100

    Update page.tsx

--- copy/app/projects/page.tsx
+++ copy/app/projects/page.tsx
@@ -167,7 +167,8 @@ export default async function ProjectsPage() {
             </Link>
           </Card>
 
-   <Card>
+
+         <Card>
             <Link href={`https://cvchat.fmac.ng/`}>
               <article className="relative w-full h-full p-4 md:p-8">
                 <div className="flex items-center justify-between gap-2">
@@ -186,6 +187,35 @@ export default async function ProjectsPage() {
                 </h2>
                 <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
                   {"AI CV screening with gemini."}
+                </p>
+                <div className="absolute bottom-4 md:bottom-8">
+                  {/* <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
+                    Read more <span aria-hidden="true">&rarr;</span>
+                  </p> */}
+                </div>
+              </article>
+            </Link>
+          </Card>
+
+          <Card>
+            <Link href={`https://newcvchat.fmac.ng/`}>
+              <article className="relative w-full h-full p-4 md:p-8">
+                <div className="flex items-center justify-between gap-2">
+                  <div className="text-xs text-zinc-100"></div>
+                  {/* <span className="flex items-center gap-1 text-xs text-zinc-500">
+                    <Eye className="w-4 h-4" />{" "}
+                    {Intl.NumberFormat("en-US", { notation: "compact" }).format(0)}
+                  </span> */}
+                </div>
+
+                <h2
+                  id="featured-post"
+                  className="mt-4 text-3xl font-bold text-slate-500 group-hover:text-slate-300 sm:text-4xl font-display"
+                >
+                  {"Improved CV chat"}
+                </h2>
+                <p className="mt-4 leading-8 duration-150 text-zinc-400 group-hover:text-zinc-300">
+                  {"AI CV screening with multiple models."}
                 </p>
                 <div className="absolute bottom-4 md:bottom-8">
                   {/* <p className="hidden text-zinc-200 hover:text-zinc-50 lg:block">
